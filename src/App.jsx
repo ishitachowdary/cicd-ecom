0a57ec53169377f75e6aab3ce4839a8bf13f5bef
ishitachowdary <ishitasai923@gmail.com>
2025-08-26 12:24:49 +0530

    Set correct base path for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-   <Router basename="/ecom-app">
+   <Router basename="/cid-ecom">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
